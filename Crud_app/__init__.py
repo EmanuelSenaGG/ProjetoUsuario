e9ec2ed2ed25060843560a8ee6a1f075a88b1817
12a34b123 <senamanu.ms@gmail.com>
2023-10-07 23:46:02 -0300

    Erro CORS

--- Crud_app/__init__.py
+++ Crud_app/__init__.py
@@ -2,5 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+# Configurar o CORS com opções adicionais
+CORS(app, resources={r"/api/*": {"origins": "*"}}, methods=["GET", "POST"], allow_headers=["Content-Type"])
+
 from Crud_app.routes import usuario  # Importe as rotas após criar a instância do Flask
